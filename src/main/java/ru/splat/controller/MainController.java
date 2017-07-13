3787cc2bbba0dc59803d0bd4e41f8c97bc0cf4b5
SpireM <vadim.a.mazitov@gmail.com>
2017-07-13 09:20:16 +0300

    add demo gif

--- src/main/java/ru/splat/controller/MainController.java
+++ src/main/java/ru/splat/controller/MainController.java
@@ -67,7 +67,6 @@ public class MainController {
     @PostMapping("/rename_node")
     public @ResponseBody boolean renameNode(@RequestBody final Node node)
     {
-        System.out.println(node);
         return nodeService.renameNode(node);
     }
 }
